f1cd56845c9dd6ccc668f16c825a09634c651f6a
NikSevenEleven <kursdz@mail.ru>
2022-07-15 06:59:12 +0300

    15.07

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,18 +5,38 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-<div>
-        <div>
+<div class="container">
+        <div class="row">
             <nav>
-                <ul>
-                    <li><a href="{{route('main.index')}}">Main</a></li>
-                    <li><a href="{{route('post.index')}}">Posts</a></li>
-                    <li><a href="{{route('about.index')}}">About</a></li>
-                    <li><a href="{{route('contact.index')}}">Contacts</a></li>
-                </ul>
+                <nav class="navbar navbar-expand-lg bg-light">
+                    <div class="container-fluid">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarText">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="{{route('main.index')}}">Main</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('about.index')}}">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{route('contact.index')}}">Contacts</a>
+                                </li>
+                            </ul>
+                            <span class="navbar-text">
+      </span>
+                        </div>
+                    </div>
+                </nav>
             </nav>
         </div>
         @yield('content')
