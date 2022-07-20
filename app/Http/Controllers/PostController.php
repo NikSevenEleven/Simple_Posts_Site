52f3d3b7e0ea047ff658c66435fad66fd1b8c8bd
NikSevenEleven <kursdz@mail.ru>
2022-07-20 11:44:00 +0300

    eight

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -9,9 +10,11 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts=Post::all();
-        return view('post.index', compact('posts'));
+
+        $posts=Post::find(1);
+         dd($posts->category);
     }
+
 
     public function create()
     {
