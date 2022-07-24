f6e993c2c321e51ed8d667d8110be63e17dbc6ad
NikSevenEleven <kursdz@mail.ru>
2022-07-24 23:36:39 +0300

    eleven

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -6,24 +6,40 @@
         @csrf
         <div class="col-md-4">
             <label for="title" class="form-label">Title</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="Title">
+            <input type="text" name="title" class="form-control" id="title" placeholder="Title"
+            value="{{old('title')}}">
+            @error('title')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="col-12">
             <label for="Content" class="form-label">Content</label>
-            <textarea name="content" class="form-control" id="Content" placeholder="Content"></textarea>
+            <textarea name="content" class="form-control" id="Content" placeholder="Content">{{old('content')}}</textarea>
+            @error('content')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="col-12">
             <label for="image" class="form-label">Image</label>
-            <input type="text" name="image" class="form-control" id="image" placeholder="Image">
+            <input type="text" name="image" class="form-control" id="image" placeholder="Image"
+                   value="{{old('image')}}">
+            @error('image')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="col-12">
             <label for="category" class="form-label">Category</label>
             <select class="form-select" aria-label="Default select example" id="category" name="category_id">
                 @foreach($categories as $category)
                      <option
+                         {{old('category_id')==$category->id?'selected':''}}
+
                          value="{{$category->id}}">{{$category->title}}</option>
                 @endforeach
             </select>
+            @error('category_id')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="form-group">
         <label for="tags" class="form-label">Tags</label>
@@ -33,6 +49,9 @@
                         value="{{$tag->id}}">{{$tag->title}}</option>
             @endforeach
         </select>
+            @error('tags[]')
+            <p class="text-danger">{{$message}}</p>
+            @enderror
         </div>
         <div class="col-1">
         <button type="submit" class="btn btn-primary">create</button>
